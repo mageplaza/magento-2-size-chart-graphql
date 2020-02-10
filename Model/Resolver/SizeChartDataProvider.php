ee597c9f90a932edfdc2e873520f98e27c8b17a6
prime <trungbd@mageplaza.com>
2020-02-10 16:28:43 +0700

    graphql

---
+++ Model/Resolver/SizeChartDataProvider.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_SizeChartGraphQl
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+declare(strict_types=1);
+
+namespace Mageplaza\SizeChartGraphQl\Model\Resolver;
+
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Mageplaza\SizeChart\Helper\Data;
+use Magento\Cms\Model\Template\FilterProvider;
+
+/**
+ * Class SizeChartDataProvider
+ * @package Mageplaza\SizeChartGraphQl\Model\Resolver
+ */
+class SizeChartDataProvider implements ResolverInterface
+{
+    /**
+     * @var Data
+     */
+    protected $helperData;
+
+    /**
+     * @var FilterProvider
+     */
+    protected $filterProvider;
+
+    /**
+     * SizeChartDataProvider constructor.
+     *
+     * @param Data $helperData
+     * @param FilterProvider $filterProvider
+     */
+    public function __construct(
+        Data $helperData,
+        FilterProvider $filterProvider
+    ) {
+        $this->helperData     = $helperData;
+        $this->filterProvider = $filterProvider;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        if (!$this->helperData->isEnabled()) {
+            return null;
+        }
+
+        /* @var $product ProductInterface */
+        $product = $value['model'];
+        $rules   = $this->helperData->getRuleCollection();
+
+        foreach ($rules as $rule) {
+            $rule->setRuleContent($this->filterProvider->getPageFilter()->filter($rule->getRuleContent()));
+
+            if ($rule->getConditions()->validate($product)) {
+                return Data::jsonEncode($rule);
+            }
+        }
+
+        return null;
+    }
+}
